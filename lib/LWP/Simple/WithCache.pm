b678f9ede8e20f51f92b9c76407fd0a291dc7916
Yen-Ming Lee (李彥明) <leeym@leeym.com>
2008-11-20 05:12:51 +0000

    Import of LEEYM/LWP-Simple-WithCache-0.02 from CPAN.
    gitpan-cpan-distribution: LWP-Simple-WithCache
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         LEEYM/LWP-Simple-WithCache-0.02.tar.gz
    gitpan-cpan-author:       LEEYM
    gitpan-cpan-maturity:     released

--- lib/LWP/Simple/WithCache.pm
+++ lib/LWP/Simple/WithCache.pm
@@ -3,7 +3,7 @@ package LWP::Simple::WithCache;
 use strict;
 use LWP::Simple;
 use LWP::UserAgent::WithCache;
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 $LWP::Simple::ua = new LWP::UserAgent::WithCache;
 $LWP::Simple::ua->agent("LWP::Simple::WithCache/$VERSION");
